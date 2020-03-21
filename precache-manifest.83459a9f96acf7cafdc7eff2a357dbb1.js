ac021c84b47acb4ea696c59e01a1ed4eee201201
brarver <brarver@yahoo.com>
2020-03-21 15:58:43 -0600

    Updates

---
+++ precache-manifest.83459a9f96acf7cafdc7eff2a357dbb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea01dfeaac4f874bf41f54a604e3c10",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "8548bb623e83f20a8be0",
+    "url": "/clickygame/static/css/main.4df87328.chunk.css"
+  },
+  {
+    "revision": "cb55f51dccc7b2ca66ac",
+    "url": "/clickygame/static/js/2.14c8cde0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame/static/js/2.14c8cde0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8548bb623e83f20a8be0",
+    "url": "/clickygame/static/js/main.4754d34a.chunk.js"
+  },
+  {
+    "revision": "f5211b3e66d21586d164",
+    "url": "/clickygame/static/js/runtime-main.cacafc75.js"
+  },
+  {
+    "revision": "6a9f92165a777789197752d135657b1e",
+    "url": "/clickygame/static/media/greenburst.6a9f9216.webp"
+  }
+]);
